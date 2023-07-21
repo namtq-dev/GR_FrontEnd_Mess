c982c33f7f5d070d583f94c497dc450553aabe87
namtq184161 <quangnam260800@gmail.com>
2023-07-21 22:02:16 +0700

    feat: persist state after refresh

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -1,8 +1,30 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userSlice from './features/userSlice';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import createFilter from 'redux-persist-transform-filter';
+
+const saveUserOnlyFilter = createFilter('user', ['user']);
+
+// data persist after refresh
+const persistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['user'],
+  transforms: [saveUserOnlyFilter],
+};
 
 const rootReducer = combineReducers({
   user: userSlice,
 });
 
-export const store = configureStore({ reducer: rootReducer, devTools: true });
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false }),
+  devTools: true,
+});
+
+export const persistor = persistStore(store);
