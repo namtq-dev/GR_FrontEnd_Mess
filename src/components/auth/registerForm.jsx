ff245722e96b7e407b6dded7ae16f800be597484
namtq184161 <quangnam260800@gmail.com>
2023-07-21 23:00:03 +0700

    feat: react-hook-form and yup

--- src/components/auth/registerForm.jsx
+++ src/components/auth/registerForm.jsx
@@ -1,4 +1,19 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { registerSchema } from '../../helpers/validation';
+
 export default function RegisterForm() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(registerSchema),
+  });
+
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div className="h-screen w-full flex items-center justify-center overflow-hidden">
       <div className="max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl">
@@ -6,7 +21,10 @@ export default function RegisterForm() {
           <h2 className="mt-6 text-3xl font-bold">Welcome</h2>
           <p className="mt-2 text-sm">Sign up</p>
         </div>
-        <form className="mt-6 space-y-6"></form>
+        <form onSubmit={handleSubmit(onSubmit)} className="mt-6 space-y-6">
+          <input type="text" {...register('name')} />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     </div>
   );
