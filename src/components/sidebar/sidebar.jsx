869e89417c9694ab188460e0748f1fa34884b5ca
namtq184161 <quangnam260800@gmail.com>
2023-07-22 22:36:40 +0700

    feat: search bar

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
 import { SidebarHeader } from './header';
 import Notifications from './notifications';
+import { Search } from './search';
 
 export default function Sidebar() {
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="w-[40%] h-full select-none">
       <SidebarHeader />
       <Notifications />
+      <Search searchResultsLength={searchResults.length} />
     </div>
   );
 }
