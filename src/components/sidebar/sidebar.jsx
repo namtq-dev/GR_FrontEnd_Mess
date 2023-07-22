fcb031a4200d854fef513fb7f752f3fa160e297e
namtq184161 <quangnam260800@gmail.com>
2023-07-22 22:11:52 +0700

    feat: sidebar notifications

--- src/components/sidebar/sidebar.jsx
+++ src/components/sidebar/sidebar.jsx
@@ -1,9 +1,11 @@
 import { SidebarHeader } from './header';
+import Notifications from './notifications';
 
 export default function Sidebar() {
   return (
     <div className="w-[40%] h-full select-none">
       <SidebarHeader />
+      <Notifications />
     </div>
   );
 }
