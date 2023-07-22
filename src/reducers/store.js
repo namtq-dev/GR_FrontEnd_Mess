82beea3d0aa55cc9fb5aec7afecff9729145a70a
namtq184161 <quangnam260800@gmail.com>
2023-07-22 23:02:21 +0700

    feat: chatSlice

--- src/reducers/store.js
+++ src/reducers/store.js
@@ -3,6 +3,7 @@ import userSlice from './features/userSlice';
 import { persistReducer, persistStore } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import createFilter from 'redux-persist-transform-filter';
+import chatSlice from './features/chatSlice';
 
 const saveUserOnlyFilter = createFilter('user', ['user']);
 
@@ -16,6 +17,7 @@ const persistConfig = {
 
 const rootReducer = combineReducers({
   user: userSlice,
+  chat: chatSlice,
 });
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
