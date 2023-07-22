05edef366fe81a61e85cc8c567239e23c8a9a461
namtq184161 <quangnam260800@gmail.com>
2023-07-22 15:43:31 +0700

    feat: protected routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 import Home from './pages/home';
 import Login from './pages/login';
 import Register from './pages/register';
+import { useSelector } from 'react-redux';
 
 function App() {
+  const { user } = useSelector((state) => state.user);
+
   return (
     <div className="dark">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} exact></Route>
-          <Route path="/login" element={<Login />} exact></Route>
-          <Route path="/register" element={<Register />} exact></Route>
+          <Route
+            path="/"
+            element={!!user.loginToken ? <Home /> : <Navigate to="/login" />}
+            exact
+          ></Route>
+          <Route
+            path="/login"
+            element={!user.loginToken ? <Login /> : <Navigate to="/" />}
+            exact
+          ></Route>
+          <Route
+            path="/register"
+            element={!user.loginToken ? <Register /> : <Navigate to="/" />}
+            exact
+          ></Route>
         </Routes>
       </Router>
     </div>
