f5e37b70b2c798cd97b26fbc758474126e005e6b
namtq184161 <quangnam260800@gmail.com>
2023-07-23 09:22:34 +0700

    feat: logout

--- src/components/sidebar/header/menu.jsx
+++ src/components/sidebar/header/menu.jsx
@@ -1,4 +1,9 @@
+import { useDispatch } from 'react-redux';
+import { logout } from '../../../reducers/features/userSlice';
+
 export default function Menu() {
+  const dispatch = useDispatch();
+
   return (
     <>
       <div className="absolute right-1 z-50 dark:bg-dark_bg_2 dark:text-dark_text_1 shadow-md w-52">
@@ -17,7 +22,7 @@ export default function Menu() {
           </li>
           <li
             className="py-3 pl-5 cursor-pointer hover:bg-dark_bg_3"
-            // onClick={() => dispatch(logout())}
+            onClick={() => dispatch(logout())}
           >
             <span>Logout</span>
           </li>
