47d0c50d60133642abec33e9839a418e1c69ce84
namtq184161 <quangnam260800@gmail.com>
2023-07-24 21:10:30 +0700

    feat: show message with image/video files

--- src/components/chat/messages/chatMessages.jsx
+++ src/components/chat/messages/chatMessages.jsx
@@ -2,6 +2,7 @@ import { useSelector } from 'react-redux';
 import Message from './message';
 import { useEffect, useRef } from 'react';
 import Typing from './typing';
+import MessageFile from './files/messageFile';
 
 export default function ChatMessages({ typing }) {
   const { messages, activeConversation } = useSelector((state) => state.chat);
@@ -25,11 +26,27 @@ export default function ChatMessages({ typing }) {
       <div className="scrollbar overflow_scrollbar overflow-auto py-2 px-[5%]">
         {messages &&
           messages.map((message) => (
-            <Message
-              message={message}
-              key={message._id}
-              mine={user.id === message.sender._id}
-            />
+            <>
+              {/* Files */}
+              {message.files?.length > 0
+                ? message.files.map((file) => (
+                    <MessageFile
+                      fileItem={file}
+                      message={message}
+                      key={message._id}
+                      mine={user.id === message.sender._id}
+                    />
+                  ))
+                : null}
+              {/* Text */}
+              {message.message?.length > 0 ? (
+                <Message
+                  message={message}
+                  key={message._id}
+                  mine={user.id === message.sender._id}
+                />
+              ) : null}
+            </>
           ))}
         {typing === activeConversation._id ? <Typing /> : null}
         <div className="mt-2" ref={endRef}></div>
---
+++ src/components/chat/messages/files/messageFile.jsx
@@ -0,0 +1,39 @@
+import moment from 'moment';
+import TriangleIcon from '../../../../svg/Triangle';
+import ImageVideoFile from './imageVideoFile';
+
+export default function MessageFile({ fileItem, message, mine }) {
+  const { file, type } = fileItem;
+
+  return (
+    <div
+      className={`w-full flex mt-2 space-x-3 max-w-xs ${
+        mine ? 'ml-auto justify-end ' : ''
+      }`}
+    >
+      <div>
+        <div
+          className={`relative h-full dark:text-dark_text_1 p-1 rounded-lg
+    ${mine ? 'border-[3px] border-green_3' : 'dark:bg-dark_bg_2'}
+    `}
+        >
+          <p className="h-full text-sm">
+            {type === 'IMAGE' || type === 'VIDEO' ? (
+              <ImageVideoFile url={file.secure_url} type={type} />
+            ) : (
+              ''
+            )}
+          </p>
+          <span className="absolute right-1.5 bottom-1.5 text-xs text-dark_text_5 leading-none">
+            {moment(message.createdAt).format('HH:mm')}
+          </span>
+          {!mine ? (
+            <span>
+              <TriangleIcon className="dark:fill-dark_bg_2 rotate-[60deg] absolute top-[-5px] -left-1.5" />
+            </span>
+          ) : null}
+        </div>
+      </div>
+    </div>
+  );
+}
