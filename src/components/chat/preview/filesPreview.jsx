73bfb4c8c3a5a9aab88c4d2d6a6d6b3a0f510748
namtq184161 <quangnam260800@gmail.com>
2023-07-24 17:10:17 +0700

    feat: file preview - input

--- src/components/chat/preview/filesPreview.jsx
+++ src/components/chat/preview/filesPreview.jsx
@@ -5,13 +5,15 @@ import Input from './input';
 import HandleFiles from './handleFiles';
 
 export default function FilesPreview() {
+  const [message, setMessage] = useState('');
+
   return (
     <div className="relative py-2 w-full flex items-center justify-center">
       <div className="w-full flex flex-col items-center">
         <Header />
         <FileViewer />
         <div className="w-full flex flex-col items-center">
-          <Input />
+          <Input message={message} setMessage={setMessage} />
           <HandleFiles />
         </div>
       </div>
