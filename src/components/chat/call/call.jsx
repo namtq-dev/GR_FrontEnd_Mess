5a687c2e109be287b0be8f7bebab9ce5e6cc096a
namtq184161 <quangnam260800@gmail.com>
2023-07-25 00:10:57 +0700

    feat: call actions UI

--- src/components/chat/call/call.jsx
+++ src/components/chat/call/call.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+import CallActions from './callActions';
 import Header from './header';
 import Ringing from './ringing';
 import VideoArea from './videoArea';
@@ -5,17 +7,20 @@ import VideoArea from './videoArea';
 export default function Call({ call, setCall, callAccepted }) {
   const { incomingCall, callEnded } = call;
 
-  console.log(incomingCall, callAccepted);
+  const [showCallActions, setSHowCallActions] = useState(false);
 
   return (
     <div
       className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
                   w-[350px] h-[550px] z-10 rounded-2xl overflow-hidden callbg`}
+      onMouseOver={() => setSHowCallActions(true)}
+      onMouseOut={() => setSHowCallActions(false)}
     >
       <div>
         <div>
           <Header />
           <VideoArea name="Quang Viet" />
+          {showCallActions ? <CallActions /> : null}
         </div>
       </div>
       {/* Call popup */}
